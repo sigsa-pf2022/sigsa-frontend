79cf85079768ef3b2913be38d2328b4eb1179182
Matias Bruno <matiasbruno97@gmail.com>
2025-05-26 18:28:40 -0300

    Chore: Remove blank space

--- src/app/components/appointments-item-list/appointments-item-list.component.spec.ts
+++ src/app/components/appointments-item-list/appointments-item-list.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { AppointmentsItemListComponent } from './appointments-item-list.component';
 
 describe('AppointmentsItemListComponent', () => {
